c5a24ffb171f670df4afdbaceffdca35b3dd2581
Anton Vynychenko <kinid@ukr.net>
2025-03-16 10:21:11 +0100

    Solution2

--- app/main.py
+++ app/main.py
@@ -3,12 +3,11 @@ import os
 
 def copy_file(command: str) -> None:
     command = command.split(" ")
-    if len(command) == 3:
+    if len(command) == 3 and command[0] == "cp":
         source, target = command[1], command[2]
         if os.path.exists(source):
-            if command[0] == "cp":
-                if source != target:
-                    with (open(source, "r") as file_in,
-                          open(target, "a") as file_out):
-                        for line in file_in:
-                            file_out.write(line)
+            if source != target:
+                with (open(source, "r") as file_in,
+                      open(target, "w") as file_out):
+                    for line in file_in:
+                        file_out.write(line)
