fb2b51e692f20bd6c3e259af6247fa49f5623bab
Anton Vynychenko <kinid@ukr.net>
2025-03-16 10:18:33 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    command = command.split(" ")
+    if len(command) == 3:
+        source, target = command[1], command[2]
+        if os.path.exists(source):
+            if command[0] == "cp":
+                if source != target:
+                    with (open(source, "r") as file_in,
+                          open(target, "a") as file_out):
+                        for line in file_in:
+                            file_out.write(line)
